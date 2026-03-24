ea2cec9c68d68f70ba4820814b110e4d0d885228
michealcalins <michealmicheal2467@gmail.com>
2026-03-24 23:22:32 +0530

    homepage created

---
+++ frontend/codespark/src/homepage.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./homepage.css";
+import heroImg from "./assets/heroimg.png";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      
+      <header className="navbar">
+        <div className="logo">CodeSpark</div>
+        <nav>
+          <a href="#">HOME</a>
+          <a href="#">COURSES</a>
+          <a href="#">CAREER</a>
+          <a href="#">INTERNSHIP</a>
+          <a href="#">ABOUT</a>
+        </nav>
+        <button className="enroll-btn">ENROLL NOW</button>
+      </header>
+
+      <section className="hero">
+        <div className="hero-left">
+          <h1>
+            Launch Your Tech <br />
+            Career <span>With CodeSpark</span>
+          </h1>
+
+          <p>
+            Master in-demand skills, work on real projects, and launch your IT career.
+          </p>
+
+          <div className="buttons">
+            <button className="primary-btn">Start Learning →</button>
+            <button className="secondary-btn">Explore Courses</button>
+          </div>
+        </div>
+
+        <div className="hero-right">
+         <img src={heroImg} alt="hero" />
+        </div>
+      </section>
+
+    </div>
+  );
+}
+
+export default Homepage;
